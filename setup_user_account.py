d1aff3f1419994be6b470d8dd44a0569e91c9053
Brang Mai <brangmai100@gmail.com>
2024-10-29 19:52:49 -0400

    User profile and movie lists added

--- setup_user_account.py
+++ setup_user_account.py
@@ -1,5 +1,6 @@
 from user import sign_up, log_in 
 from search_movie import search
+from tables import User
 
 def setup_user(session):
     while True:
@@ -15,9 +16,44 @@ def setup_user(session):
         elif choice == '2':
             user = log_in(session)
             if user is not None:
-                search(session)
+                profile(user, session)
         elif choice == '0':
             print("Goodbye!")
             break
         else:
             print("Invalid option. Try again.")
+
+
+def profile(user, session):
+    user_profile = session.query(User).filter(User.username == user).first()
+    watching_movies = user_profile.watching_list
+    future_movies = user_profile.to_watch_list
+    watched_movies = user_profile.watched_list
+    print("-----------------------------------------------------------------------------")
+    print("CURRENTLY WATCHING MOVIES")
+    print("-----------------------------------------------------------------------------")
+    if not watching_movies:
+        print("No movies currently watching.")
+    else:
+        for movie in watching_movies:
+            print(f"id: {movie.id}, title: {movie.title}")
+    print("-----------------------------------------------------------------------------")
+    print("MOVIES TO WATCH IN THE FUTURE")
+    print("-----------------------------------------------------------------------------")
+    if not future_movies:
+        print("No movies to watch in the future.")
+    else:
+        for movie in future_movies:
+            print(f"id: {movie.id}, title: {movie.title}")
+    print("-----------------------------------------------------------------------------")
+    print("WATHCHED MOVIES")
+    print("-----------------------------------------------------------------------------")
+    if not watched_movies:
+        print("No watched movies listed.")
+    else:
+        for movie in future_movies:
+            print(f"id: {movie.id}, title: {movie.title}")
+
+    search_movie = input("Do you want search Movies? Enter 'y' for 'Yes', 'n' for No: ")
+    if search_movie.lower() == 'y':
+        search(session)
